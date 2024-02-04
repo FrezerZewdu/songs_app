bdf1d1c34a09d40696c9cd0dc62670c08a624773
Frezer <frezerzewdu@gmail.com>
2024-02-04 13:09:17 +0300

    mongo connection and error handler added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const dotenv = require('dotenv').config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/error-handler");
+connectDB();
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -9,7 +14,8 @@ app.use(cors());
 // Routes imported
 const songsRoute = require("./routes/songsRoute");
 
-app.use('api/songs', songsRoute);
+app.use('/api/songs', songsRoute);
+app.use(errorHandler);
 
 var port = process.env.PORT || 8090;
 app.listen(port);
